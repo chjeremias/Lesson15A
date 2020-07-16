516a07943b64549beafac80e8be0adb33473ad28
chjeremias <56939781+chjeremias@users.noreply.github.com>
2020-07-15 23:33:20 -0400

    vuejs

---
+++ my-first-app/vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    publicPath: process.env.NODE_ENV === "production" ? '/chjeremias/WEB215/Lesson15A/my-first-app/dist' : '/',
+}
---
+++ my-vue-project/vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    publicPath: process.env.NODE_ENV === "production" ? '/chjeremias/WEB215/Lesson15A/my-vue-project/dist' : '/',
+}
